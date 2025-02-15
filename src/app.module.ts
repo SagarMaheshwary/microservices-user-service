50d3819aee7a2d1ba3290609c3f9abe62a707054
Sagar Maheshwary <sagarmaheshwary31@gmail.com>
2025-02-15 19:53:25 +0500

    Loading system variables if .env file does exists

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { DatabaseModule } from "./modules/database/database.module";
 import { UserModule } from "./modules/user/user.module";
 import { HealthModule } from "./modules/health/health.module";
 import { PrometheusModule } from "./modules/prometheus/prometheus.module";
+import { existsSync } from "fs";
+import { join } from "path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
+      ignoreEnvFile: !existsSync(join(__dirname, "..", ".env")),
     }),
     DatabaseModule,
     UserModule,
